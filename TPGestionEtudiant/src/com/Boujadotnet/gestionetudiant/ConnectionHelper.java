555683a7771b310ac5e57a73d3dbc05a8accc5b8
SLD <SLD@DEVELOPSLD>
2025-02-06 13:06:40 +0100

    the last

---
+++ TPGestionEtudiant/src/com/Boujadotnet/gestionetudiant/ConnectionHelper.java
@@ -0,0 +1,17 @@
+package com.Boujadotnet.gestionetudiant;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionHelper {
+	
+	private static final String URL = "jdbc:mysql://localhost/gestion_etudiants";
+    private static final String USER = "root"; // Remplacez par votre utilisateur MySQL
+    private static final String PASSWORD = ""; // Remplacez par votre mot de passe MySQL
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+    
+}
